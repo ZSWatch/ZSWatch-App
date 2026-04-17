a9db1827bf9854aefe0d2bb857b9a485bbe63e99
Jakob Krantz <mail@jakobkrantz.se>
2026-04-17 22:05:18 +0200

    chore: drop generated desktop plugin registrants from PR
    Restore Flutter desktop generated plugin registrant files to their main
    branch versions so they are not included in the feature PR.
    
    Also add explicit ignore rules documenting that these generated desktop
    registrant files should not be committed from local regeneration.

--- zswatch_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ zswatch_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,7 +8,6 @@ import Foundation
 import audio_session
 import ffmpeg_kit_flutter_new_min
 import file_picker
-import file_selector_macos
 import flutter_blue_plus_darwin
 import flutter_secure_storage_macos
 import geolocator_apple
@@ -16,7 +15,6 @@ import just_audio
 import package_info_plus
 import path_provider_foundation
 import record_macos
-import share_plus
 import shared_preferences_foundation
 import sqlite3_flutter_libs
 import url_launcher_macos
@@ -26,7 +24,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   FFmpegKitFlutterPlugin.register(with: registry.registrar(forPlugin: "FFmpegKitFlutterPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
-  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
@@ -34,7 +31,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
-  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
--- zswatch_app/windows/flutter/generated_plugins.cmake
+++ zswatch_app/windows/flutter/generated_plugins.cmake
@@ -3,12 +3,10 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  file_selector_windows
   flutter_secure_storage_windows
   geolocator_windows
   permission_handler_windows
   record_windows
-  share_plus
   sqlite3_flutter_libs
   url_launcher_windows
 )
